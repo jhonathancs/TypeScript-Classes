c32a6055ed71ca99bdffb91841a07867ade3c0f2
Jhonathan Costa <jhonathan_jcs@hotmail.com>
2023-06-22 13:31:52 -0400

    2.2
    completed

--- modulo-2/2.1/src/components/Header.tsx
+++ modulo-2/2.1/src/components/Header.tsx
@@ -1,29 +1,28 @@
 function Header() {
-    const firstName = 'Jhon';
-    const lastName = 'Souza';
-  
-    function luckyNumber() {
-      return Math.floor(Math.random() * 60 + 1);
-    }
-  
-    return (
-      <div>
-        <h1 className="greeting">
-          Olá {`${firstName} ${lastName}`}
-          <img src="https://i.imgur.com/rqvLd3q.png" alt="" />
-        </h1>
-        <h2 className="subtitle">Seus números da sorte da Mega-Sena são:</h2>
-        <ul>
-          <li>{luckyNumber()}</li>
-          <li>{luckyNumber()}</li>
-          <li>{luckyNumber()}</li>
-          <li>{luckyNumber()}</li>
-          <li>{luckyNumber()}</li>
-          <li>{luckyNumber()}</li>
-        </ul>
-      </div>
-    );
+  const firstName = "Jhon";
+  const lastName = "Souza";
+
+  function luckyNumber() {
+    return Math.floor(Math.random() * 60 + 1);
   }
-  
-  export default Header;
-  
+
+  return (
+    <div>
+      <h1 className="greeting">
+        Olá {`${firstName} ${lastName}`}
+        <img src="https://i.imgur.com/rqvLd3q.png" alt="" />
+      </h1>
+      <h2 className="subtitle">Seus números da sorte da Mega-Sena são:</h2>
+      <ul>
+        <li>{luckyNumber()}</li>
+        <li>{luckyNumber()}</li>
+        <li>{luckyNumber()}</li>
+        <li>{luckyNumber()}</li>
+        <li>{luckyNumber()}</li>
+        <li>{luckyNumber()}</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Header;
