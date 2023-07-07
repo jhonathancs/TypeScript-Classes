a5e6c1c76851369b865f4ba35d1186449c8a19b5
Jhonathan Costa <jhonathan_jcs@hotmail.com>
2023-07-06 21:46:00 -0400

    3.1
    react-router

---
+++ modulo-3/3.1/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/home';
+import CoffeeList from './pages/coffee-list';
+
+function App() {
+
+  return (
+    <Routes>
+      <Route path="/" element={ <Home /> } />
+      <Route path="/coffees" element={ <CoffeeList /> } />
+    </Routes>
+)}
+
+export default App
